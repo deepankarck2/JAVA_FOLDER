7d8da0192163252982eae7b951258559ba77c91e
Deepankar <dip2leo@gmail.com>
2022-07-24 20:23:59 -0400

    added BST

--- BST_intro.java
+++ BST_intro.java
@@ -1,23 +1,47 @@
-public class BST_intro {
-    
-    //Individual Nodes 
-    public class Node{
-        int data;
-        Node left;
-        Node right; 
+ //Individual Nodes 
+class Node{
+    int data;
+    Node left;
+    Node right; 
+}
+
+class BST{
+
+    public Node createNewNode(int k){
+        Node a = new Node();
+        a.data=k;
+        a.left=null;
+        a.right = null; 
+        
+        return a; 
     }
 
-    public class BST{
-        
-        //Internal Methods 
-        public void insert(Node node){
-
+    public Node insert(Node node, int val){
+        if(node == null){
+            return createNewNode(val); 
         }
+        else{
+            if( val <= node.data){
+                node.left = insert(node, val); //Recursively doing it.
+            }
 
-
+            else if(val > node.data){
+                node.right = insert(node, val); 
+            }
+        }
+        return node; 
     }
+}
 
-    public static void main(String[] args) {
+public class BST_intro {
+
+   public static void main(String[] args) {
         
+        BST myBst = new BST();
+        Node root = null; 
+
+        root = myBst.insert(root, 8);
+        root = myBst.insert(root, 10);
+
     }
 }
