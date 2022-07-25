450912b42376bfb8adac6b3080e67032e43c479d
Deepankar <dip2leo@gmail.com>
2022-07-24 20:07:45 -0400

    added BST

---
+++ BST_intro.java
@@ -0,0 +1,23 @@
+public class BST_intro {
+    
+    //Individual Nodes 
+    public class Node{
+        int data;
+        Node left;
+        Node right; 
+    }
+
+    public class BST{
+        
+        //Internal Methods 
+        public void insert(Node node){
+
+        }
+
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
+}
