85aaeddcde062ad872b0de160224ffbe40780570
Deepankar <dip2leo@gmail.com>
2022-08-27 10:28:05 -0400

    added arraylist

---
+++ arrayList.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> newList = new ArrayList<>(Arrays.asList(3,4,5,6,2));
+        System.out.println(newList);
+    }
+}
