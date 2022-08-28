ad5a8b2459fdaf6d03675d9e28bc87fcefee610a
Deepankar <dip2leo@gmail.com>
2022-08-27 20:51:23 -0400

    added binary search

---
+++ binary_search.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+public class binary_search {
+    static int binary_search1(int[] list){
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] list = {1,3,4,8,10,18,100};
+        
+        System.out.println(binary_search1(list));
+
+    }
+}
