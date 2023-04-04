1c997c8efbf50416629ee1e8a9d389660c2cdf05
Deepankar <dip2leo@gmail.com>
2023-04-03 21:07:03 -0400

    added String methods

--- arrays.java
+++ arrays.java
@@ -2,30 +2,34 @@ import java.util.Arrays;
 
 public class arrays {
     public static void main(String[] args) {
-        String[] colors = new String[5]; //Creating an array with 5 elements 
+        String[] colors = new String[5]; // Creating an array with 5 elements
         colors[0] = "Purple";
-        System.out.println(colors[0].charAt(2)); //r 
+        System.out.println(colors[0].charAt(2)); // r
 
-        //int numbers[] = {100,200};  
-        int[] numbers2 = {300,400}; 
-                                    //BOTH Works above 
-        int [] numbers3 = new int[7];
-        
-        //int num2_size = numbers2.length;  //To get the length of an array
-
-        for(int i:numbers2){
-            System.out.print(i+" ");
+        // BOTH Works below:
+        // int numbers[] = {100,200};
+        int[] numbers2 = { 300, 400 };
+        for (int i : numbers2) {
+            System.out.print(i + " ");
         }
+
+        int[] numbers3 = new int[7];
+        for (int i : numbers3) {
+            System.out.print(i + " ");
+        }
+
+        // Size:
+        int num2_size = numbers2.length; // To get the length of an array
+        System.out.println(num2_size);
+
         System.out.println("\n");
-        //STREAM 
+        // STREAM
         Arrays.stream(colors).forEach(System.out::println);
 
-        // Utility Methods 
+        // Utility Methods
         // Arrays. <-- this will show differnet util methods in array
-        
-        
 
         /* 2D Array */
-        
+
     }
 }
--- myString.java
+++ myString.java
@@ -13,7 +13,35 @@ public class myString {
             System.out.println(i);
         }
 
-        System.out.println("Sorted String: " + sort(s));
+        System.out.println("Sorted String: " + sort(s)); // Java has no internal sort. See below.
+        System.out.println(s.charAt(0));
+
+        // Length
+        System.out.println(s.length());
+
+        // Array of String
+        String[] colors = new String[5]; // Creating an array with 5 elements
+        colors[0] = "Purple";
+        System.out.println(colors[0].charAt(2)); // r
+        String[] given = new String[] { "hello", "world" };
+
+        // substring(int beginIndex, int endIndex)
+        System.out.println(s.substring(0, 4));
+
+        // Uppercare --> toUpperCase(), toLowerCase(),
+        String new_s = s.toUpperCase();
+        System.out.println(new_s);
+
+        // Strip:
+        String str = "   Hello, World!   ";
+        String strippedStr = str.strip();
+        System.out.println(strippedStr); // Output: "Hello, World!"
+
+        // Split
+        String str2 = "Hello, World!";
+        String[] strippedStr2 = str2.split(" ");
+        System.out.println(strippedStr2[0]); // Output: "Hello, World!"
+
     }
 
     /* String doesn't have a sort method built in */
